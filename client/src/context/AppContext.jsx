d285c0842a15cb12cda1873f7594faa73bdb75e4
Peter Byun <byunpeterj@gmail.com>
2021-01-12 11:55:43 -0500

    connect react app to express route and render news articles

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,17 +3,13 @@ import React, { createContext, useState } from 'react';
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const [contextMessage, setContextMessage] = useState('');
-
-  const contextMethod = () => {
-    setContextMessage('Hello from client/src/context/AppContext.jsx');
-  };
+  const [loading, setLoading] = useState(false);
 
   return (
     <AppContext.Provider
       value={{
-        contextMessage,
-        contextMethod
+        loading,
+        setLoading
       }}
     >
       {children}
